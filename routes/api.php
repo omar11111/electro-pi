480fc8644a7cb499241c5ca2e5f1a1224af7ae91
Omar ALGalfy <31424768+omar11111@users.noreply.github.com>
2026-08-02 06:19:14 +0300

    make needed routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+
+    Route::apiResource('projects', ProjectController::class);
+
+    // shallow() => GET/POST use /projects/{project}/tasks,
+    // while show/update/destroy collapse to /tasks/{task} directly.
+    Route::apiResource('projects.tasks', TaskController::class)->shallow();
+});
